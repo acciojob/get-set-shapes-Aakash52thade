003f57193ae55fb05e8a2ccffcb2e3da2955773c
Aakash52thade <117812701+Aakash52thade@users.noreply.github.com>
2025-07-22 07:16:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,43 +1,46 @@
-//complete this code
+// Rectangle class
 class Rectangle {
-	constructor(width, height){
-		this._width = width;
-		this._height = height;
-	}
-	get width(){
-	  return this._width;
-	}
+  constructor(width, height) {
+    this._width = width;
+    this._height = height;
+  }
 
-	get height(){
-       return this._height;
-	}
+  get width() {
+    return this._width;
+  }
 
-	 getArea(){
-		return this._width * this._height;
-	}
+  get height() {
+    return this._height;
+  }
+
+  getArea() {
+    return this._width * this._height;
+  }
 }
 
+// Square class inheriting from Rectangle
 class Square extends Rectangle {
-	constructor(side){
-		super(side, side);
-		this._side = side;
-	}
-	getPerimeter(side){
-		return this._side * 4;
-	}
+  constructor(side) {
+    super(side, side); // width = side, height = side
+    this._side = side;
+  }
+
+  getPerimeter() {
+    return this._side * 4;
+  }
 }
 
-const reactangle = new Rectangle(5,10);
-console.log(reactangle.width);
-console.log(reactangle.height);
-console.log(reactangle.getArea());
+// Test code
+const rectangle = new Rectangle(5, 10);
+console.log(rectangle.width);        // Output: 5
+console.log(rectangle.height);       // Output: 10
+console.log(rectangle.getArea());    // Output: 50
 
 const square = new Square(7);
-console.log(Square.width);
-console.log(Square.height);
-console.log(Square.getArea());
-console.log(Square.getPerimeter());
-
+console.log(square.width);           // Output: 7
+console.log(square.height);          // Output: 7
+console.log(square.getArea());       // Output: 49
+console.log(square.getPerimeter());  // Output: 28
 
 // Do not change the code below this line
 window.Rectangle = Rectangle;
